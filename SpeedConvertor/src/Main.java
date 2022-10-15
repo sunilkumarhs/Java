323fb4fd8cd89e98e9a796a6c576dfaf8ce02eba
sunilkumarhs <sunilkumarhs974117@gmail.com>
2022-10-15 19:27:52 +0530

    SpeedConvertor Challenge

---
+++ SpeedConvertor/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //SpeedConvertor speed=new SpeedConvertor();
+        SpeedConvertor.printConversion(10.5);
+        SpeedConvertor.printConversion(100.56);
+    }
+}
---
+++ SpeedConvertor/src/SpeedConvertor.java
@@ -0,0 +1,17 @@
+public class SpeedConvertor {
+    public static long toMilesPerHour(double kilometerPerHour) {
+        if (kilometerPerHour < 0) {
+            return -1;
+        }
+        return Math.round(kilometerPerHour / 1.609);
+    }
+
+    public static void printConversion(double kilometerPerHour) {
+        if (kilometerPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long milesPerHour = toMilesPerHour(kilometerPerHour);
+            System.out.println(kilometerPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+    }
+}
