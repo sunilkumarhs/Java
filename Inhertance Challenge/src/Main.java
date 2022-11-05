d9da6c2aee2ad7308858e933f5951519ad2712db
sunilkumarhs <sunilkumarhs974117@gmail.com>
2022-11-06 01:14:50 +0530

    Inheritance Challenge 2

--- Inhertance Challenge/src/Main.java
+++ Inhertance Challenge/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        //Calculating the area of the circle and area, volume of the  cylinder using the concept of Inheritance
         Circle circle = new Circle(3.75);
         System.out.println("circle.radius= " + circle.getRadius());
         System.out.println("circle.area= " + circle.getArea());
@@ -8,5 +9,19 @@ public class Main {
         System.out.println("cylinder.height= " + cylinder.getHeight());
         System.out.println("cylinder.area= " + cylinder.getArea());
         System.out.println("cylinder.volume= " + cylinder.getVolume());
+        System.out.println("");
+        System.out.println("------------------------------------------------------------------------");
+        System.out.println("");
+        //Calculating the area of the rectangle and area, volume of the Cuboid using the concept of Inheritance
+        Rectangle rectangle = new Rectangle(5, 10);
+        System.out.println("rectangle.width= " + rectangle.getWidth());
+        System.out.println("rectangle.length= " + rectangle.getLength());
+        System.out.println("rectangle.area= " + rectangle.getArea());
+        Cuboid cuboid = new Cuboid(5,10,5);
+        System.out.println("cuboid.width= " + cuboid.getWidth());
+        System.out.println("cuboid.length= " + cuboid.getLength());
+        System.out.println("cuboid.area= " + cuboid.getArea());
+        System.out.println("cuboid.height= " + cuboid.getHeight());
+        System.out.println("cuboid.volume= " + cuboid.getVolume());
     }
 }
---
+++ Inhertance Challenge/src/Rectangle.java
@@ -0,0 +1,29 @@
+public class Rectangle {
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+        if(width < 0) {
+            this.width = 0;
+        }else {
+            this.width = width;
+        }
+        if(length < 0) {
+            this.length = 0;
+        }else {
+            this.length = length;
+        }
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public double getLength() {
+        return this.length;
+    }
+
+    public double getArea() {
+        return width*length;
+    }
+}
