8ba2a7c1647a0f4c70631c61fc8db2981443427f
sunilkumarhs <sunilkumarhs974117@gmail.com>
2022-11-06 00:54:54 +0530

    Inheritance Challenge 1

---
+++ Inhertance Challenge/src/Circle.java
@@ -0,0 +1,19 @@
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        if(radius < 0) {
+            this.radius = 0;
+        }else {
+            this.radius = radius;
+        }
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public double getArea() {
+        return this.radius*this.radius*Math.PI;
+    }
+}
