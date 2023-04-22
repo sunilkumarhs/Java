ef98b619056cc3210203bb6adbc543b97a035901
sunilkumarhs <sunilkumarhs974117@gmail.com>
2023-04-22 20:43:29 +0530

    InterfaceChallenge

--- JavaInterfaces/src/Test.java
+++ JavaInterfaces/src/Test.java
@@ -4,15 +4,15 @@ public class Test {
 //        OrbitEarth.log("Testing"+new Satellite());
         orbit(new Satellite());
     }
-//    private static void inFlight(FlightEnabled flight) {
-//        flight.takeOff();
-//        flight.transition(FlightStages.CRUISE);
-//        flight.fly();
-//        if(flight instanceof Tracking tracks){
-//            tracks.track();
-//        }
-//        flight.land();
-//    }
+    private static void inFlight(FlightEnabled flight) {
+        flight.takeOff();
+        flight.transition(FlightStages.CRUISE);
+        flight.fly();
+        if(flight instanceof Tracking tracks){
+            tracks.track();
+        }
+        flight.land();
+    }
     private static void orbit(OrbitEarth flight) {
         flight.takeOff();
         flight.fly();
