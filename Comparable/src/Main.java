42409784c087820498ab8607d8b2e629e56bf738
sunilkumarhs <sunilkumarhs974117@gmail.com>
2023-04-28 21:10:36 +0530

    comparator

--- Comparable/src/Main.java
+++ Comparable/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,27 +23,41 @@ public class Main {
         System.out.println("A:"+(int)'A'+" "+"a:"+(int)'a');
 
         Student sunil = new Student("Sunil");
-        Student[] students = {new Student("Sunil"),new Student("Charan"),new Student("sunil"),new Student("Ganesh")};
+        Student[] students = {new Student("Sunil"),new Student("Charan"),new Student("seena"),new Student("Ganesh")};
 
         Arrays.sort(students);
         System.out.println(Arrays.toString(students));
+        int result= sunil.compareTo(new Student("Ganesh"));
+        System.out.println("compare Result : "+result);
     }
 }
-class Student implements Comparable{
+class Student implements Comparable<Student>{
     private String name;
+    private static int LAST_ID = 1000;
+    private static Random random = new Random();
+    private int id;
+    protected double gpa;
 
     public Student(String name) {
         this.name = name;
+        id = LAST_ID++;
+        gpa = random.nextDouble(1.0,4);
     }
 
     @Override
     public String toString() {
-        return name;
+
+        return "%d - %s (%.2f)".formatted(id,name,gpa);
     }
 
+//    @Override
+//    public int compareTo(Object o) {
+//        Student others = (Student) o;
+//        return name.compareTo(others.name);
+//    }
+
     @Override
-    public int compareTo(Object o) {
-        Student others = (Student) o;
-        return name.compareTo(others.name);
+    public int compareTo(Student o) {
+        return Integer.valueOf(id).compareTo(Integer.valueOf(o.id));
     }
 }
